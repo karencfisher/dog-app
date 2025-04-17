73af051685edd35902a8b66da3170f94c3dc16d0
karencfisher <karen@karencfisher.com>
2025-04-16 18:45:23 -0600

    Added map to shelter!

--- DoggyMatch/app/src/main/java/com/example/doggymatch/ui/screens/OrgMapScreen.kt
+++ DoggyMatch/app/src/main/java/com/example/doggymatch/ui/screens/OrgMapScreen.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.doggymatch.models.Organizations
 import com.example.doggymatch.ui.components.OsmMapView
 
 @Composable
