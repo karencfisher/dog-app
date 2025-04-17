be023995f3d0dc77139527d89df2568facfd56ad
karencfisher <karen@karencfisher.com>
2025-04-17 14:41:50 -0600

    added picture to home screen

--- DoggyMatch/app/src/main/java/com/example/doggymatch/ui/screens/HomeScreen.kt
+++ DoggyMatch/app/src/main/java/com/example/doggymatch/ui/screens/HomeScreen.kt
@@ -1,10 +1,12 @@
 package com.example.doggymatch.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -12,8 +14,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.doggymatch.R
 
 @Composable
 fun HomeScreen(
@@ -77,6 +82,14 @@ fun HomeScreen(
                     textAlign = TextAlign.Center
                 )
             }
+            Image(
+                painter = painterResource(id = R.drawable.adopt_me_please),
+                contentDescription = "Dog image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top=16.dp),
+                contentScale = ContentScale.FillWidth
+            )
         }
     }
 }
